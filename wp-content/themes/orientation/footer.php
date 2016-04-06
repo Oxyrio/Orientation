f12b2defff14612f36d17be1a0bc7b9491f6ce47
Oxyrio <axelandayme@hotmail.fr>
2016-04-06 16:20:20 +0200

    Ajout Wordpress

---
+++ wp-content/themes/orientation/footer.php
@@ -0,0 +1,42 @@
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="ask" style="padding-bottom: 10%">
+                    <h1>Posez nous une question :</h1>
+                    <input type="text" placeholder="Nom, prénom...">
+                    <input type="text" placeholder="Nom, prénom..."><br>
+                    <button name="button" style="color: black; float: right">Envoyer</button>
+                </div>
+                <p style="text-align:justify">Orientation.fr<br>
+                Le site de l'orientation scolaire personnalisée<br>
+                - Orientation - Orientation scolaire <br>
+                - Tests d'orientation<br>
+                - Coaching scolaire<br></p>
+            </div>
+
+            <div class="col-md-6 col-sm-6 ">
+                <p style="text-align:justify">Orientation<br>
+                Agrément n° NO7/01/09/F078S002,<br>
+                conformément aux dispositions de l'article<br>
+                L 7232-1-2-3-4-5 du code du travail,<br>
+                pour la fourniture de services aux personnes<br></p>
+
+            </div>
+
+            <div class="" style="padding-top: 2%">
+                <h4>
+                    <strong>©Copyright 2016 Tous droits réservés</strong>
+                </h4>
+                <nav id="navigation_footer">
+                    <?php wp_nav_menu(array('theme_location' => 'secondaire')); ?>
+                </nav>
+        </div>
+    </div>
+</footer>
+
+</body>
+<?php // wp_footer(); ?>
+</html>
---
+++ wp-content/themes/orientation/index.php
@@ -0,0 +1,239 @@
+<?php get_header(); ?>
+
+
+<!-- Header -->
+<header id="top" class="header">
+    <nav id="navigation">
+        <img src="<?php bloginfo('template_directory'); ?>/img/logo3.png">
+        <?php wp_nav_menu(array('theme_location' => 'principal')); ?>
+    </nav>
+ <div class="text-vertical-center">
+            <h2 style="color: #FFFFFF; font-weight: bold">Envie de vous réorienter ?</h2>
+            <h2 style="font-weight:bold; padding-left:5%">Trouvez votre</h2>
+            <h1><?php
+                bloginfo( $show );
+            ?></h1>
+            <h2>avec nos <b>Coachs professionnels !</b></h2>
+            <br>
+            <br>
+           <!-- <a href="#about" class="btn btn-dark btn-lg" style="padding-left: ">Qui sommes-nous ?</a>-->
+        </div>
+    </header>
+
+    <section id="control" class="control bg-primary">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <div class="row">
+                        <h3>Prenez le contrôle de votre <b>avenir<b> !</h3>
+                            <div class="black"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="orientation" class="second bg-primary">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <div class="row">
+                            <div class="col-md-7 col-sm-6">
+                                <div class="service-item">
+                                    <div class="orange">
+                                        <h4>
+                                            <strong><?php bloginfo( $show ); ?></strong>, qu'est ce que c'est ?
+                                        </h4>
+                                    </div>
+                                    <p>
+                                        Dès notre plus jeune âge, on nous demande ce que l’on aimerait faire plus tard dans la vie.
+                                        Pour beaucoup, arrivé en terminal, nous ne savons toujours pas où s’orienter et choisissons
+                                        des choix souvent par défaut, sur les conseils des professeurs ou de la famille.
+                                        Au final arrivée en fin d’année on se dit que ce n’est pas ce qui nous plait forcément et qu’on
+                                        aimerait faire autre chose, quelque chose qui nous correspondrait plus, un domaine dans lequel on
+                                        pourrait s’épanouir dans son travail. L’orientation est très importante car cela détermine plusieurs
+                                        années d’études supérieurs dans lesquelles il faudra montrer le maximum de soi afin d’obtenir son diplôme.
+                                        Nous vous aidons dans votre orienta on à y voir plus clair, et surtout déterminer quelle orientation on vous
+                                        conviendrait le mieux !
+                                    </p>
+                                </div>
+                            </div>
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="milf">
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <section>
+        <div class="container third">
+            <div class="row">
+                <div class="hr">
+
+                </div>
+
+                <div class="test">
+                    <h1>Profitez de notre <strong style="color: #d65453;">expertise</strong></h1>
+                </div>
+
+                <div class="hr">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="advice">
+            <div class="advicetwo">
+                <div class="container fourth">
+                    <div class="row">
+                        <h1 style="text-align: center">Conseils et astuces</h1>
+                    </div>
+                </div>
+                    <div class="row">
+                        <div class="bug">
+                            <p>Nous vous aidons dans tout type de situation,<br> que vous soyez en recherche de stage,<br>
+                                en recherche d’emploi ou tout simplement<br> en recherche de nouvelle orienta on, nos coachs<br>
+                                professionnels vous apporteront<br> les meilleurs conseils et astuces afin que vous obteniez<br>
+                                ce que vous voulez.
+                            </p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section>
+        <div class="container fifth">
+            <div class="row">
+                <div class="color">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="dude">
+
+                        </div>
+                    </div>
+                </div>
+
+                    <div class="col-md-6 col-sm-6">
+                        <div class="bug">
+                            <div class="title">
+                                <h1>Suivi personnalisé</h1>
+                            </div>
+
+                            <p>Un bilan vous est proposé gratuitement<br>
+                                afin d’en savoir plus sur votre personnalité,<br>
+                                vos mo va ons ainsi que vous intérêts professionnels.<br>
+                                Les tests sont u lisés fréquemment dans le cadre des bilans<br>
+                                d’orienta on professionnelles alors n’a endez plus et<br>
+                                faites votre bilan dès maintenant !
+                            </p>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container fifth">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="dudetwo">
+
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-sm-6">
+                    <div class="bug">
+                        <div class="title">
+                            <h1>Coach expérimentés</h1>
+                        </div>
+
+                        <p>Nos consultants sont spécialisés dans le coaching professionnel<br>
+                            et vous accompagnent tout le long de votre carrière.<br>
+                            Que vous soyez étudiants ou jeunes diplômés,<br>
+                            nous vous aidons à y voir plus clair dans votre orientation<br>
+                            et me re en places de nouvelles stratégies pour que vous<br>
+                            réussissiez dans ce que vous voulez. Notre coaching peut aussi<br>
+                            vous permettre de saisie de nouvelles opportunités de carrière.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container third">
+            <div class="row">
+                <div class="hr">
+
+                </div>
+
+                <div class="test">
+                    <h1><strong style="color: #d65453;">Votre situation</strong> actuelle ?</h1>
+                </div>
+
+                <div class="hr">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="trait">
+            <div class ="container sixth">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                        <div class="picone"></div>
+                        <h1 style="text-align: center"><strong style="color: #d65453;">BAC</strong> + 1 ?</h1>
+                        <p style="text-align: center">Perdu après le bac ? Lorem Ipsum !</p>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="pictwo"></div>
+                        <h1 style="text-align: center"><strong style="color: #d65453;">BAC</strong> + 3 ?</h1>
+                        <p style="text-align: center">Réorientation en bac + 3</p>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="picthree"></div>
+                        <h1 style="text-align: center"><strong style="color: #d65453;">BAC</strong> + 5 ?</h1>
+                        <p style="text-align: center">Fin des études et réorientation ? Oui c’est possible !</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container seventh">
+            <div class="row">
+                <h1  style="text-align: center">Avec</h1>
+                <div class="col-md-6 col-sm-6">
+                    <div class="leonard">
+
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <div class="leonarda">
+
+                    </div>
+                </div>
+        </div>
+    </section>
+
+<!-- Portfolio -->
+    
+
+<?php get_footer(); ?>
